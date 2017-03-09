7e04da30c9981902bebec31fd9789219b11e572a
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-09 17:54:47 -0500

    Create initial project for pcm
    #12909: As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

---
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/PcmApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s.pcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmApplication.class, args);
+	}
+}
