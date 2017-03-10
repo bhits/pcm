35d21cddd92a09f1997117db6cf10fe33c79df32
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-09 21:59:02 -0500

    Add initial configuration for PCM to work with config-server
    #12909: As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

--- pcm/src/main/java/gov/samhsa/c2s/pcm/PcmApplication.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/PcmApplication.java
@@ -2,11 +2,15 @@ package gov.samhsa.c2s.pcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class PcmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PcmApplication.class, args);
+    }
 }
