5b20fa3cfe396aef12885a7b76536cf625a1e48c
CindyLiFei <cindy.li@feisystems.com>
2017-03-20 10:51:38 -0400

    Confifure FHIR properties and beans
    #12909:As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

--- pcm/src/main/java/gov/samhsa/c2s/pcm/config/PcmProperties.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/config/PcmProperties.java
@@ -8,6 +8,8 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,4 +23,29 @@ public class PcmProperties {
 
     @NotEmpty
     private List<String> supportedProviderSystems = new ArrayList<>();
+
+    @NotNull
+    private Consent consent;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Consent {
+
+        @Valid
+        private Publish publish;
+
+        @Data
+        public static class Publish {
+            @NotNull
+            private boolean enabled;
+
+            @NotEmpty
+            private String serverUrl;
+
+            @NotEmpty
+            private String clientSocketTimeoutInMs;
+        }
+    }
 }
