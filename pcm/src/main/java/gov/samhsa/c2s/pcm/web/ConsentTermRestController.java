7e040f02001d1360fb5602342f8ef41ca012d1eb
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-24 10:18:48 -0400

    Change RequestParam to PathVariable for getting consentAttestationTerm
    #12909: As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

--- pcm/src/main/java/gov/samhsa/c2s/pcm/web/ConsentTermRestController.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/web/ConsentTermRestController.java
@@ -4,7 +4,7 @@ import gov.samhsa.c2s.pcm.service.ConsentService;
 import gov.samhsa.c2s.pcm.service.dto.ConsentTermDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -16,12 +16,12 @@ public class ConsentTermRestController {
     private ConsentService consentService;
 
     @GetMapping("/consentAttestationTerm")
-    public ConsentTermDto getConsentAttestationTerm(@RequestParam Optional<Long> id) {
+    public ConsentTermDto getConsentAttestationTerm(@PathVariable Optional<Long> id) {
         return consentService.getConsentAttestationTerm(id);
     }
 
     @GetMapping("/consentRevocationTerm")
-    public ConsentTermDto getConsentRevocationTerm(@RequestParam Optional<Long> id) {
+    public ConsentTermDto getConsentRevocationTerm(@PathVariable Optional<Long> id) {
         return consentService.getConsentRevocationTerm(id);
     }
 }
