9e94f2d4e1a0c9958d6098843e56c40c662e2a4e
burcakulug <burcak.ulug@feisystems.com>
2017-03-23 22:54:04 -0400

    Add consent stage to the detailed consent dto in the get consents endpoint
    #12909:As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

--- pcm/src/main/java/gov/samhsa/c2s/pcm/service/ConsentServiceImpl.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/service/ConsentServiceImpl.java
@@ -194,6 +194,7 @@ public class ConsentServiceImpl implements ConsentService {
                 .sharePurposes(sharePurposes)
                 .startDate(consent.getStartDate())
                 .endDate(consent.getEndDate())
+                .consentStage(consent.getConsentStage())
                 .build();
     }
 
