1ebdeeefcc08bd3d96f4a8a93d750fa1a79d1a74
burcakulug <burcak.ulug@feisystems.com>
2017-03-24 16:38:01 -0400

    Use flyway for the database migration and add sample insert scripts for certain lookups
    #12909:As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

---
+++ pcm-db-sample/insert_purposes.sql
@@ -0,0 +1,10 @@
+INSERT INTO purpose VALUES (1,
+                            'To perform one or more operations on information for the provision of health care.',
+                            'Treatment',
+                            'http://hl7.org/fhir/v3/ActReason', 'TREAT');
+INSERT INTO purpose VALUES (2,
+                            'To perform one or more operations on information for conducting financial or contractual activities related to payment for the provision of health care.',
+                            'Healthcare Payment', 'http://hl7.org/fhir/v3/ActReason', 'HPAYMT');
+INSERT INTO purpose VALUES (3,
+                            'To perform one or more operations on information for conducting scientific investigations to obtain health care knowledge.',
+                            'Healthcare Research', 'http://hl7.org/fhir/v3/ActReason', 'HRESCH');
