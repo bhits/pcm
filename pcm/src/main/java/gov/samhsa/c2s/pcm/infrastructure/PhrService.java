9ae2cc50c392e63f26674a32e0eb480517aed3aa
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-03-25 02:06:34 -0400

    Add default temporary method for patient identifiers
    #12909 - As a developer, I will have updated the PCM API, to support the logic share and integrated with new componentized UI [3.0.0]

--- pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/PhrService.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/PhrService.java
@@ -1,14 +1,20 @@
 package gov.samhsa.c2s.pcm.infrastructure;
 
 import gov.samhsa.c2s.pcm.infrastructure.dto.PatientDto;
+import gov.samhsa.c2s.pcm.infrastructure.dto.PatientIdentifierDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @FeignClient(name = "phr")
+@Service
 public interface PhrService {
+
 
     // FIXME: call actual PHR service instead of returning mock data after PHR is adjusted as a core service
     @RequestMapping(value = "/patients", method = RequestMethod.GET)
@@ -24,6 +30,24 @@ public interface PhrService {
                 .stateCode("MD")
                 .zip("22222")
                 .birthDate(new Date())
+                .patientIdentifiers(gePatientIdentifierDtos())
                 .build();
+    }
+
+    default List<PatientIdentifierDto> gePatientIdentifierDtos(){
+
+        PatientIdentifierDto mrnPatientId = PatientIdentifierDto.builder()
+                .system("https://bhits.github.io/consent2share/")
+                .value("PID-4")
+                .label("MRN")
+                .build();
+        PatientIdentifierDto ssnPatientId = PatientIdentifierDto.builder()
+                .system("http://hl7.org/fhir/sid/us-ssn")
+                .value("123456789")
+                .label("SSN")
+                .build();
+
+        return Arrays.asList(mrnPatientId, ssnPatientId);
+
     }
 }
