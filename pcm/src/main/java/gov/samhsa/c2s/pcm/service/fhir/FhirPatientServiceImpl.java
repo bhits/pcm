0b1c4808b9e0dd8afb5aeb2076a48474cba82bfa
CindyLiFei <cindy.li@feisystems.com>
2017-05-03 12:59:42 -0400

    Fix exception when generating fhir patient
    #13512: As a developer, I will have integrated UMS with C2S components

--- pcm/src/main/java/gov/samhsa/c2s/pcm/service/fhir/FhirPatientServiceImpl.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/service/fhir/FhirPatientServiceImpl.java
@@ -3,9 +3,8 @@ package gov.samhsa.c2s.pcm.service.fhir;
 
 import gov.samhsa.c2s.pcm.config.FhirProperties;
 import gov.samhsa.c2s.pcm.infrastructure.dto.PatientDto;
-import gov.samhsa.c2s.pcm.infrastructure.dto.PatientIdentifierDto;
-import gov.samhsa.c2s.pcm.infrastructure.dto.TelecomDto;
 import org.hl7.fhir.dstu3.model.ContactPoint;
+import org.hl7.fhir.dstu3.model.Enumerations;
 import org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Identifier;
@@ -14,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
-import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -22,8 +20,6 @@ public class FhirPatientServiceImpl implements FhirPatientService {
 
     @Autowired
     private FhirProperties fhirProperties;
-
-
 
     @Override
     public Patient getFhirPatient(PatientDto patientDto) {
@@ -39,9 +35,6 @@ public class FhirPatientServiceImpl implements FhirPatientService {
             //setting mandatory fields
 
             fhirPatient.addName().setFamily(patientDto.getLastName()).addGiven(patientDto.getFirstName());
-            Optional<TelecomDto> email= patientDto.getTelecoms().stream().filter(telecomDto -> telecomDto.getSystem().equalsIgnoreCase(ContactPoint.ContactPointSystem.EMAIL.toString())).findFirst();
-            if(email.isPresent())
-            fhirPatient.addTelecom().setValue(email.get().getValue()).setSystem(ContactPoint.ContactPointSystem.EMAIL);
             fhirPatient.setBirthDate(Date.valueOf(patientDto.getBirthDate()));
             fhirPatient.setGender(getPatientGender.apply(patientDto.getGenderCode()));
             fhirPatient.setActive(true);
@@ -50,7 +43,13 @@ public class FhirPatientServiceImpl implements FhirPatientService {
             setIdentifiers(fhirPatient, patientDto);
 
             //optional fields
-            fhirPatient.addAddress().addLine(patientDto.getAddresses().get(0).getLine1()).setCity(patientDto.getAddresses().get(0).getCity()).setState(patientDto.getAddresses().get(0).getStateCode()).setPostalCode(patientDto.getAddresses().get(0).getPostalCode());
+            patientDto.getAddresses().stream().forEach(addressDto ->
+                    fhirPatient.addAddress().addLine(addressDto.getLine1()).addLine(addressDto.getLine2()).setCity(addressDto.getCity()).setState(addressDto.getStateCode()).setPostalCode(addressDto.getPostalCode())
+            );
+
+            patientDto.getTelecoms().stream().forEach(telecomDto ->
+                    fhirPatient.addTelecom().setSystem(ContactPoint.ContactPointSystem.valueOf(telecomDto.getSystem())).setUse(ContactPoint.ContactPointUse.valueOf(telecomDto.getUse())).setValue(telecomDto.getValue())
+            );
             return fhirPatient;
         }
     };
@@ -59,43 +58,42 @@ public class FhirPatientServiceImpl implements FhirPatientService {
     private void setIdentifiers(Patient patient, PatientDto patientDto) {
 
 
-        for(PatientIdentifierDto pidDto : patientDto.getPatientIdentifiers()) {
-            if(pidDto.getSystem().equalsIgnoreCase(fhirProperties.getMrn().getSystem())) {
-                //setting patient mrn
-                patient.addIdentifier().setSystem(fhirProperties.getMrn().getSystem())
-                        .setUse(Identifier.IdentifierUse.OFFICIAL).setValue(pidDto.getValue());
-                patient.setId(new IdType(pidDto.getValue()));
-            } else if(pidDto.getSystem().equalsIgnoreCase(fhirProperties.getSsn().getSystem())) {
-                String ssnValue =  pidDto.getValue();
-                // setting ssn value
-                if (null != ssnValue && !ssnValue.isEmpty())
-                    patient.addIdentifier().setSystem(fhirProperties.getSsn().getSystem())
-                            .setValue(ssnValue);
-            }
-        }
-        Optional<TelecomDto> telephone=patientDto.getTelecoms().stream().filter(telecomDto -> telecomDto.getSystem().equalsIgnoreCase(ContactPoint.ContactPointSystem.PHONE.toString())).findFirst();
-        if (telephone.isPresent())
+        //setting patient mrn
+        patient.addIdentifier().setSystem(fhirProperties.getMrn().getSystem())
+                .setUse(Identifier.IdentifierUse.OFFICIAL).setValue(patientDto.getMrn());
 
-            patient.addTelecom().setValue(telephone.get().getValue()).setSystem(ContactPoint.ContactPointSystem.PHONE);
+        patient.setId(new IdType(patientDto.getMrn()));
+
+        // setting ssn value
+        String ssnValue = patientDto.getSocialSecurityNumber();
+        if (null != ssnValue && !ssnValue.isEmpty())
+            patient.addIdentifier().setSystem(fhirProperties.getSsn().getSystem())
+                    .setValue(ssnValue);
     }
 
     Function<String, AdministrativeGender> getPatientGender = new Function<String, AdministrativeGender>() {
         @Override
-        public AdministrativeGender apply(String codeString) {
-            if (codeString != null && !"".equals(codeString) || codeString != null && !"".equals(codeString)) {
-                if ("male".equalsIgnoreCase(codeString) || "M".equalsIgnoreCase(codeString)) {
-                    return AdministrativeGender.MALE;
-                } else if ("female".equalsIgnoreCase(codeString) || "F".equalsIgnoreCase(codeString)) {
-                    return AdministrativeGender.FEMALE;
-                } else if ("other".equalsIgnoreCase(codeString) || "O".equalsIgnoreCase(codeString)) {
-                    return AdministrativeGender.OTHER;
-                } else if ("unknown".equalsIgnoreCase(codeString) || "UN".equalsIgnoreCase(codeString)) {
-                    return AdministrativeGender.UNKNOWN;
-                } else {
-                    throw new IllegalArgumentException("Unknown AdministrativeGender code \'" + codeString + "\'");
-                }
-            } else {
-                return null;
+        public Enumerations.AdministrativeGender apply(String codeString) {
+            switch (codeString.toUpperCase()) {
+                case "MALE":
+                    return Enumerations.AdministrativeGender.MALE;
+                case "M":
+                    return Enumerations.AdministrativeGender.MALE;
+                case "FEMALE":
+                    return Enumerations.AdministrativeGender.FEMALE;
+                case "F":
+                    return Enumerations.AdministrativeGender.FEMALE;
+                case "OTHER":
+                    return Enumerations.AdministrativeGender.OTHER;
+                case "O":
+                    return Enumerations.AdministrativeGender.OTHER;
+                case "UNKNOWN":
+                    return Enumerations.AdministrativeGender.UNKNOWN;
+                case "UN":
+                    return Enumerations.AdministrativeGender.UNKNOWN;
+                default:
+                    return Enumerations.AdministrativeGender.UNKNOWN;
+
             }
         }
     };
