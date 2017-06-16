a2d939e90560b6f1d344f70ffe3a8df5e8c81a94
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-16 09:40:36 -0400

    Add missing import
    #14009 - As a developer I will have implemented backend to support - Segmentation/Upload/download patient health record workflow

--- pcm/src/main/java/gov/samhsa/c2s/pcm/service/ConsentService.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/service/ConsentService.java
@@ -6,6 +6,7 @@ import gov.samhsa.c2s.pcm.service.dto.ConsentRevocationDto;
 import gov.samhsa.c2s.pcm.service.dto.ConsentTermDto;
 import gov.samhsa.c2s.pcm.service.dto.DetailedConsentDto;
 import gov.samhsa.c2s.pcm.service.dto.SensitivityCategoryDto;
+import gov.samhsa.c2s.pcm.service.dto.XacmlRequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
