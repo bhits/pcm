64db2198c2c6effcea530d16c99730486de33672
Shruti Rao <shruti.rao@feisystems.com>
2017-06-25 20:15:49 -0400

    Remove unused method
    #236: Update Provider UI and Backend for Consent Signed by Provider

--- pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/UmsService.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/UmsService.java
@@ -1,7 +1,6 @@
 package gov.samhsa.c2s.pcm.infrastructure;
 
 import gov.samhsa.c2s.pcm.infrastructure.dto.PatientDto;
-import gov.samhsa.c2s.pcm.infrastructure.dto.PatientIdentifierDto;
 import gov.samhsa.c2s.pcm.infrastructure.dto.UserDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Service;
@@ -9,32 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Arrays;
-import java.util.List;
-
 @FeignClient(name = "ums")
 @Service
 public interface UmsService {
 
     @RequestMapping(value = "/patients/{mrn}", method = RequestMethod.GET)
     PatientDto getPatientProfile(@PathVariable("mrn") String mrn);
-
-    default List<PatientIdentifierDto> gePatientIdentifierDtos(){
-
-        PatientIdentifierDto mrnPatientId = PatientIdentifierDto.builder()
-                .system("https://bhits.github.io/consent2share/")
-                .value("PID-4")
-                .label("MRN")
-                .build();
-        PatientIdentifierDto ssnPatientId = PatientIdentifierDto.builder()
-                .system("http://hl7.org/fhir/sid/us-ssn")
-                .value("123456789")
-                .label("SSN")
-                .build();
-
-        return Arrays.asList(mrnPatientId, ssnPatientId);
-
-    }
 
     @RequestMapping(value = "/authId/{userAuthId}", method = RequestMethod.GET)
     UserDto getUserById(@PathVariable("userAuthId") String userAuthId);
