c13237f3c78facaa07e75f67a8091aa0a83b7c0f
Shruti Rao <shruti.rao@feisystems.com>
2017-07-06 10:30:48 -0400

    Remove redundant modifier
    #373: Update Provider UI and Backend for Consent Signed by Provider

--- pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/pdf/ITextPdfService.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/pdf/ITextPdfService.java
@@ -44,7 +44,7 @@ public interface ITextPdfService {
 
     String formatDate(Date aDate);
 
-    public String formatLocalDate(LocalDate aDate);
+    String formatLocalDate(LocalDate aDate);
 
     String getFullName(String firstName, String lastName);
 }
