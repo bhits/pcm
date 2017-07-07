18cad5701ca2e946c0a22c68bb3bf68a82aa6315
Shruti Rao <shruti.rao@feisystems.com>
2017-07-07 11:40:47 -0400

    Add space for signature in an appropriate place and also add appropriate comments
    #373:Update Provider UI and Backend for Consent Signed by Provider

--- pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/pdf/ConsentPdfGeneratorImpl.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/infrastructure/pdf/ConsentPdfGeneratorImpl.java
@@ -114,10 +114,11 @@ public class ConsentPdfGeneratorImpl implements ConsentPdfGenerator {
                 String firstName = attesterUserDto.get().getFirstName();
                 String lastName = attesterUserDto.get().getLastName();
                 email = attesterUserDto.get().getTelecoms().stream().filter(telecomDto -> telecomDto.getSystem().equalsIgnoreCase(EMAIL)).findFirst().get().getValue();
-                document.add(new Paragraph(" "));
-                document.add(iTextPdfService.createSpaceForSignatureByPatientAndOtherRole("Provider", isSigned));
-                //TODO: Ideally, "Provider" should come from a DTO or request
+                //TODO: Ideally, "Provider"/role should come either from a DTO or a request
                 document.add(iTextPdfService.createNonPatientSigningDetailsTable("Provider", firstName, lastName, email, isSigned, attestedOn));
+                document.add(new Paragraph(" "));
+                //TODO: Ideally, "Provider"/role should come either from a DTO or a request
+                document.add(iTextPdfService.createSpaceForSignatureByPatientAndOtherRole("Provider", isSigned));
             } else {
                 document.add(iTextPdfService.createPatientSigningDetailsTable(patientProfile.getFirstName(), patientProfile.getLastName(), email, isSigned, attestedOn));
             }
