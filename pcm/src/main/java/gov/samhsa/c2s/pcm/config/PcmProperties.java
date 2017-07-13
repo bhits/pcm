c307df34f7a76f48c521ccb108e01d2c62f2f610
Shruti Rao <shruti.rao@feisystems.com>
2017-07-13 13:05:18 -0400

    Add additional configuration for separate patient and provider revocation terms
    446:As a developer, I will have changed the attestation and revocation terms on the UI and PDF based on the customer feedback

--- pcm/src/main/java/gov/samhsa/c2s/pcm/config/PcmProperties.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/config/PcmProperties.java
@@ -6,11 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Configuration;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -38,6 +36,18 @@ public class PcmProperties {
 
         @Valid
         private Pagination pagination = new Pagination();
+
+        @NotNull
+        private Long activePatientAttestationTermId;
+
+        @NotNull
+        private Long activePatientRevocationTermId;
+
+        @NotNull
+        private Long activeProviderAttestationTermId;
+
+        @NotNull
+        private Long activeProviderRevocationTermId;
 
         @Valid
         private Publish publish;
