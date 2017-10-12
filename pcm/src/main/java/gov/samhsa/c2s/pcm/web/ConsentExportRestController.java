e6fa10098827fe8f01ddd42b6d825628a1b0b7b3
burcakulug <burcak.ulug@feisystems.com>
2017-10-12 16:38:39 -0400

    Verify XACML support for many-to-many consents and refactor code
    #16415:[Beach ball]: As a patient, I can select multiple providers for sharing consent, so that I can create one consent that shares data.

--- pcm/src/main/java/gov/samhsa/c2s/pcm/web/ConsentExportRestController.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/web/ConsentExportRestController.java
@@ -1,10 +1,9 @@
 package gov.samhsa.c2s.pcm.web;
 
-import gov.samhsa.c2s.common.consentgen.ConsentGenException;
 import gov.samhsa.c2s.pcm.service.consentexport.ConsentExportService;
 import gov.samhsa.c2s.pcm.service.dto.ConsentXacmlDto;
 import gov.samhsa.c2s.pcm.service.dto.XacmlRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +16,13 @@ public class ConsentExportRestController {
 
     private final ConsentExportService consentExportService;
 
+    @Autowired
     public ConsentExportRestController(ConsentExportService consentExportService) {
         this.consentExportService = consentExportService;
     }
 
-
     @RequestMapping(value = "/consents/export/xacml", method = RequestMethod.POST)
-    public ConsentXacmlDto exportXACMLConsent(@Valid @RequestBody XacmlRequestDto xacmlRequestDto)  {
+    public ConsentXacmlDto exportXACMLConsent(@Valid @RequestBody XacmlRequestDto xacmlRequestDto) {
         return consentExportService.exportConsent2XACML(xacmlRequestDto);
     }
 }
