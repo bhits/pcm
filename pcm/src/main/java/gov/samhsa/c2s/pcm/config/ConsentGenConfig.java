18251e341208e3e1b8c12e4aba3a0a6169c42c06
Shruti Rao <shruti.rao@feisystems.com>
2017-10-12 11:00:59 -0400

    Rename ConsentGen
    #16415: As a patient, I can select multiple providers for sharing consent, so that I can create one consent that shares data.

--- pcm/src/main/java/gov/samhsa/c2s/pcm/config/ConsentGenConfig.java
+++ pcm/src/main/java/gov/samhsa/c2s/pcm/config/ConsentGenConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ConsentgenConfig {
+public class ConsentGenConfig {
 
     @Value("${c2s.pcm.fhir.mrn.oid}")
     private String pcmOrg;
